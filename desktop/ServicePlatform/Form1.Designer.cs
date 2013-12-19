8b74a6c53e188af0112f86204a8786207a33d418
Piotr Niziniecki <pniziniecki@gmail.com>
2013-12-19 23:01:07 +0100

    simple GUI

--- desktop/ServicePlatform/Form1.Designer.cs
+++ desktop/ServicePlatform/Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace ServicePlatform {
-	partial class Form1 {
+	partial class MainForm {
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
@@ -23,12 +23,156 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.services = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.fileBrowser = new System.Windows.Forms.Button();
+			this.outputBrowser = new System.Windows.Forms.Button();
+			this.process = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.AutoSize = true;
+			this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Controls.Add(this.services, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+			this.tableLayoutPanel1.Controls.Add(this.fileBrowser, 1, 1);
+			this.tableLayoutPanel1.Controls.Add(this.outputBrowser, 1, 2);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(13, 13);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 3;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(254, 75);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// services
+			// 
+			this.services.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.services.FormattingEnabled = true;
+			this.services.Items.AddRange(new object[] {
+            "UpperCase",
+            "GrayScale"});
+			this.services.Location = new System.Drawing.Point(130, 3);
+			this.services.Name = "services";
+			this.services.Size = new System.Drawing.Size(121, 21);
+			this.services.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label1.Location = new System.Drawing.Point(3, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(121, 25);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Service";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label2.Location = new System.Drawing.Point(3, 25);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(121, 25);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Files";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label3.Location = new System.Drawing.Point(3, 50);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(121, 25);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Output folder";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// fileBrowser
+			// 
+			this.fileBrowser.Dock = System.Windows.Forms.DockStyle.Left;
+			this.fileBrowser.Location = new System.Drawing.Point(130, 28);
+			this.fileBrowser.Name = "fileBrowser";
+			this.fileBrowser.Size = new System.Drawing.Size(75, 19);
+			this.fileBrowser.TabIndex = 4;
+			this.fileBrowser.Text = "Browse";
+			this.fileBrowser.UseVisualStyleBackColor = true;
+			this.fileBrowser.Click += new System.EventHandler(this.fileBrowser_Click);
+			// 
+			// outputBrowser
+			// 
+			this.outputBrowser.Dock = System.Windows.Forms.DockStyle.Left;
+			this.outputBrowser.Location = new System.Drawing.Point(130, 53);
+			this.outputBrowser.Name = "outputBrowser";
+			this.outputBrowser.Size = new System.Drawing.Size(75, 19);
+			this.outputBrowser.TabIndex = 5;
+			this.outputBrowser.Text = "Browse";
+			this.outputBrowser.UseVisualStyleBackColor = true;
+			this.outputBrowser.Click += new System.EventHandler(this.outputBrowser_Click);
+			// 
+			// process
+			// 
+			this.process.Location = new System.Drawing.Point(197, 226);
+			this.process.Name = "process";
+			this.process.Size = new System.Drawing.Size(75, 23);
+			this.process.TabIndex = 1;
+			this.process.Text = "Process";
+			this.process.UseVisualStyleBackColor = true;
+			this.process.Click += new System.EventHandler(this.process_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.AutoSize = true;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.process);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "ServicePlatform";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox services;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button fileBrowser;
+		private System.Windows.Forms.Button outputBrowser;
+		private System.Windows.Forms.Button process;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
 	}
 }
 
--- desktop/ServicePlatform/Form1.cs
+++ desktop/ServicePlatform/Form1.cs
@@ -12,17 +12,30 @@ using System.IO;
 
 namespace ServicePlatform {
 
-	public partial class Form1 : Form {
+	public partial class MainForm : Form {
 
 		private System.Threading.Timer timer = null;
 		private Boolean detected = false;
 
-		private const String VERSION = "0.1";
+		private ArrayList files = new ArrayList();
+		private String outputFolder;
+
+		private PortableDevice device;
+		private PortableDeviceFolder servicePlatformFolder;
+
+		private const String VERSION = "0.1.1";
 		private const int WM_DEVICECHANGE = 0x0219;
 		private const int DBT_DEVNODES_CHANGED = 0x0007;
 
-		public Form1() {
+		public MainForm() {
 			InitializeComponent();
+		}
+
+		private void MainForm_Load(object sender, EventArgs e) {
+			BeginInvoke(new MethodInvoker(delegate {
+				Hide();
+			}));
+			openFileDialog1.Multiselect = true;
 		}
 
 		private void disconnectWithDevice(object state) {
@@ -42,13 +55,13 @@ namespace ServicePlatform {
 			var devices = new PortableDeviceCollection();
 			devices.Refresh();
 
-			var device = devices.First();
+			device = devices.First();
 			device.Connect();
 
 			Console.WriteLine("Connected to: " + device.FriendlyName);
 
 			var root = device.GetContents();
-			PortableDeviceFolder servicePlatformFolder = (PortableDeviceFolder)device.getServicePlatformFolder();
+			servicePlatformFolder = (PortableDeviceFolder)device.getServicePlatformFolder();
 
 			if (servicePlatformFolder == null) {
 				Console.WriteLine("Could not find ServicePlatform folder, have you installed ServicePlatform mobile app? Disconnecting...");
@@ -56,95 +69,14 @@ namespace ServicePlatform {
 				return;
 			}
 
-			Console.WriteLine("Cleaning up ServicePlatform folder...");
+			BeginInvoke(new MethodInvoker(delegate {
+				Show();
+				//MessageBox.Show("Connected to: " + device.FriendlyName);
+			}));
+
 			cleanup(device, servicePlatformFolder);
 
-			Console.WriteLine("ServicePlatform folder detected!");
-			Console.WriteLine("Which service would you like to run?");
-			Console.WriteLine("> UpperCase");
-			Console.WriteLine("Enter files which you would like to process (one file per line, blank line - start processing): ");
-
-			String line, outputFolder;
-			ArrayList files = new ArrayList();
-
-			do {
-				line = Console.ReadLine();
-				if (!line.Equals("")) {
-					files.Add(line);
-				}
-			} while (!line.Equals(""));
-
-			Console.WriteLine("Detected " + files.Count + " files");
-			Console.WriteLine("Enter output folder: ");
-			outputFolder = Console.ReadLine();
-
-			Console.WriteLine("Sending input params file to device...");
-
-			if (!File.Exists("input-params")) {
-				FileStream fs = File.Create("input-params");
-				fs.Close();
-			}
-			using (StreamWriter sw = new StreamWriter(File.Open("input-params", System.IO.FileMode.Truncate))) {
-				sw.WriteLine(VERSION);
-				sw.WriteLine("UpperCase");
-				foreach (String file in files) {
-					String[] parts = file.Split('\\');
-					sw.WriteLine(parts[parts.Length - 1]);
-				}
-			}
-
-			device.TransferContentToDevice(@Directory.GetCurrentDirectory() + "\\input-params", servicePlatformFolder.Id);
-
-			Console.WriteLine("Sending files to device...");
-
-			foreach (var item in servicePlatformFolder.Files) {
-				if (item is PortableDeviceFolder && item.Name.Equals("Input")) {
-					foreach (String file in files) {
-						device.TransferContentToDevice(@file, item.Id);
-					}
-					break;
-				}
-			}
-
-			Console.WriteLine("Initiating processing...");
-
-			if (!File.Exists("desktop-finished")) {
-				FileStream fs = File.Create("desktop-finished");
-				fs.Close();
-			}
-			device.TransferContentToDevice(@Directory.GetCurrentDirectory() + "\\desktop-finished", servicePlatformFolder.Id);
-
-			Console.WriteLine("Starting processing...");
-
-			Boolean finished = false;
-
-			do {
-
-				System.Threading.Thread.Sleep(1000);
-				device.refreshFolderContents(servicePlatformFolder);
-
-				foreach (var item in servicePlatformFolder.Files) {
-					if (item is PortableDeviceFile && item.Name.Equals("mobile-finished")) {
-						Console.WriteLine("Processing finished!");
-						finished = true;
-					}
-				}
-			} while (!finished);
-
-			Console.WriteLine("Downloading output files...");
-
-			foreach (var item in servicePlatformFolder.Files) {
-				if (item is PortableDeviceFolder && item.Name.Equals("Output")) {
-					downloadFiles(device, (PortableDeviceFolder)item, outputFolder);
-				}
-			}
-
-			Console.WriteLine("Download finished!");
-			Console.WriteLine("Cleaning up ServicePlatform folder...");
-			cleanup(device, servicePlatformFolder);
-
-			Console.WriteLine("Disconnecting from device");
-			device.Disconnect();
+			//device.Disconnect();
 		}
 
 		private void downloadFiles(PortableDevice device, PortableDeviceFolder folder, String outputFolder) {
@@ -157,6 +89,7 @@ namespace ServicePlatform {
 		}
 
 		private void cleanup(PortableDevice device, PortableDeviceFolder folder) {
+			Console.WriteLine("Cleaning up ServicePlatform folder...");
 			foreach (var item in folder.Files) {
 				if (item is PortableDeviceFile && (item.Name.Equals("input-params") || item.Name.Equals("desktop-finished") || item.Name.Equals("mobile-finished"))) {
 					device.DeleteFile((PortableDeviceFile)item);
@@ -202,6 +135,144 @@ namespace ServicePlatform {
 					timer = new System.Threading.Timer(disconnectWithDevice, null, 2000, System.Threading.Timeout.Infinite);
 				}
 			}
+		}
+
+		private void fileBrowser_Click(object sender, EventArgs e) {
+			DialogResult dr = openFileDialog1.ShowDialog();
+			if (dr == System.Windows.Forms.DialogResult.OK) {
+				files.Clear();
+				files.AddRange(openFileDialog1.FileNames);
+			}
+		}
+
+		private void outputBrowser_Click(object sender, EventArgs e) {
+			DialogResult dr = folderBrowserDialog1.ShowDialog();
+			if (dr == System.Windows.Forms.DialogResult.OK) {
+				outputFolder = folderBrowserDialog1.SelectedPath;
+			}
+		}
+
+		private void process_Click(object sender, EventArgs e) {
+
+			if (services.SelectedIndex == -1) {
+				showError("Please select service");
+				return;
+			}
+
+			if (files.Count == 0) {
+				showError("Please select files to process");
+				return;
+			}
+
+			if (outputFolder == null) {
+				showError("Please select output folder");
+				return;
+			}
+
+			Enabled = false;
+
+			transferInputFile(services.SelectedItem.ToString());
+			transferFiles();
+			initiateProcessing();
+			waitForFinish();
+			downloadFiles();
+			openOutputFolder();
+			cleanup(device, servicePlatformFolder);
+
+			services.SelectedIndex = -1;
+			outputFolder = null;
+			files.Clear();
+
+			Enabled = true;
+		}
+
+		private void showError(String error) {
+			MessageBox.Show(error, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+		}
+
+		private void transferInputFile(String serviceName) {
+
+			Console.WriteLine("Sending input params file to device...");
+
+			if (!File.Exists("input-params")) {
+				FileStream fs = File.Create("input-params");
+				fs.Close();
+			}
+			using (StreamWriter sw = new StreamWriter(File.Open("input-params", System.IO.FileMode.Truncate))) {
+				sw.WriteLine(VERSION);
+				sw.WriteLine(serviceName);
+				foreach (String file in files) {
+					String[] parts = file.Split('\\');
+					sw.WriteLine(parts[parts.Length - 1]);
+				}
+			}
+
+			device.TransferContentToDevice(@Directory.GetCurrentDirectory() + "\\input-params", servicePlatformFolder.Id);
+		}
+
+		private void transferFiles() {
+
+			Console.WriteLine("Sending files to device...");
+
+			foreach (var item in servicePlatformFolder.Files) {
+				if (item is PortableDeviceFolder && item.Name.Equals("Input")) {
+					foreach (String file in files) {
+						device.TransferContentToDevice(@file, item.Id);
+					}
+					break;
+				}
+			}
+		}
+
+		private void initiateProcessing() {
+
+			Console.WriteLine("Initiating processing...");
+
+			if (!File.Exists("desktop-finished")) {
+				FileStream fs = File.Create("desktop-finished");
+				fs.Close();
+			}
+
+			device.TransferContentToDevice(@Directory.GetCurrentDirectory() + "\\desktop-finished", servicePlatformFolder.Id);
+		}
+
+		private void waitForFinish() {
+
+			Console.WriteLine("Waiting for processing to end...");
+
+			Boolean finished = false;
+
+			do {
+
+				System.Threading.Thread.Sleep(1000);
+				device.refreshFolderContents(servicePlatformFolder);
+
+				foreach (var item in servicePlatformFolder.Files) {
+					if (item is PortableDeviceFile && item.Name.Equals("mobile-finished")) {
+						Console.WriteLine("Processing finished!");
+						finished = true;
+					}
+				}
+			} while (!finished);
+		}
+
+		private void downloadFiles() {
+
+			Console.WriteLine("Downloading output files...");
+
+			foreach (var item in servicePlatformFolder.Files) {
+				if (item is PortableDeviceFolder && item.Name.Equals("Output")) {
+					downloadFiles(device, (PortableDeviceFolder)item, outputFolder);
+				}
+			}
+
+			Console.WriteLine("Download finished!");
+		}
+
+		private void openOutputFolder() {
+			System.Diagnostics.Process prc = new System.Diagnostics.Process();
+			prc.StartInfo.FileName = outputFolder;
+			prc.Start();
 		}
 	}
 }
